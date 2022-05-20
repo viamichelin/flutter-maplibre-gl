32a6cd14c7d078c658493c51eef5c69c95f685f0
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2022-05-20 18:58:22 +0200

    test if iOS layers example still crashes

--- example/lib/layer.dart
+++ example/lib/layer.dart
@@ -26,6 +26,8 @@ class LayerState extends State {
   @override
   Widget build(BuildContext context) {
     return MaplibreMap(
+      dragEnabled: false,
+      myLocationEnabled: true,
       onMapCreated: _onMapCreated,
       onMapClick: (point, latLong) =>
           print(point.toString() + latLong.toString()),
